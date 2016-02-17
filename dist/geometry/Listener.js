6866b5c49c9d799f7d6ae1380760286b4f6787a4
jipodine <jipodine@users.noreply.github.com>
2016-02-17 15:24:36 +0100

    Listener, again for distribution.

---
+++ dist/geometry/Listener.js
@@ -0,0 +1,219 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Listener = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /**
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @fileOverview Listener.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @author Jean-Philippe.Lambert@ircam.fr
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @copyright 2016 IRCAM, Paris, France
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @license CECILL-2.1
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      */
+
+var _glMatrix = require('gl-matrix');
+
+var _glMatrix2 = _interopRequireDefault(_glMatrix);
+
+var _coordinates = require('../geometry/coordinates');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * Camera-like listener. It generates a look-at matrix from a position, a
+ * view point, and an up direction.
+ *
+ */
+
+var Listener = exports.Listener = function () {
+
+  /**
+   * Constructs a listener.
+   *
+   * @param {CoordinateSystem} [options.coordinateSystem='gl']
+   * {@link Listener#coordinateSystem}
+   * @param {Coordinates} [options.position=[0,0,0]]
+   * {@link Listener#position}
+   * @param {Coordinates} [options.up=[0,1,0]]
+   * {@link Listener#up}
+   * @param {Coordinates} [options.view=[0,0,-1]]
+   * {@link Listener#view}
+   */
+
+  function Listener() {
+    var options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+    _classCallCheck(this, Listener);
+
+    this._outdated = true;
+    this._lookAt = [];
+
+    this.coordinateSystem = options.coordinateSystem;
+
+    this._position = [];
+    this.position = typeof options.position !== 'undefined' ? options.position : (0, _coordinates.glToSystem)([], [0, 0, 0], this.coordinateSystem);
+
+    this._up = [];
+    this.up = typeof options.up !== 'undefined' ? options.up : (0, _coordinates.glToSystem)([], [0, 1, 0], this.coordinateSystem);
+
+    this._view = [];
+    this.view = typeof options.view !== 'undefined' ? options.view : (0, _coordinates.glToSystem)([], [0, 0, -1], this.coordinateSystem);
+
+    this.update();
+  }
+
+  // ------------- accessors
+
+  /**
+   * Get the current look-at matrix. Note is updated only after a call to
+   * the update method.
+   *
+   * @see {@link Listener#update}
+   *
+   * @returns {mat4} look-at matrix
+   */
+
+
+  _createClass(Listener, [{
+    key: 'update',
+
+
+    // --------- public methods
+
+    /**
+     * Updates the look-at matrix, according to the pending changes in
+     * position, view, and up.
+     *
+     * @returns {Boolean} true when at least a change occurred.
+     */
+    value: function update() {
+      var updated = this._outdated;
+      if (this._outdated) {
+        _glMatrix2.default.mat4.lookAt(this._lookAt, this._position, this._view, this._up);
+        this._outdated = false;
+      }
+
+      return updated;
+    }
+  }, {
+    key: 'lookAt',
+    get: function get() {
+      return this._lookAt;
+    }
+
+    /**
+     * Set coordinate system.
+     *
+     * @param {CoordinateSystem} [type='gl']
+     */
+
+  }, {
+    key: 'coordinateSystem',
+    set: function set(coordinateSystem) {
+      this._coordinateSystem = typeof coordinateSystem !== 'undefined' ? coordinateSystem : 'gl';
+    }
+
+    /**
+     * Get coordinate system.
+     *
+     * @returns {CoordinateSystem}
+     */
+    ,
+    get: function get() {
+      return this._coordinateSystem;
+    }
+
+    /**
+     * Set listener position. It will update the relative positions of the
+     * sources after a call to the update method.
+     *
+     * Default value is [0, 0, 0] in 'gl' coordinates.
+     *
+     * @see {@link Listener#update}
+     *
+     * @param {Coordinates} positionRequest
+     */
+
+  }, {
+    key: 'position',
+    set: function set(positionRequest) {
+      (0, _coordinates.systemToGl)(this._position, positionRequest, this._coordinateSystem);
+      this._outdated = true;
+    }
+
+    /**
+     * Get listener position.
+     *
+     * @returns {Coordinates}
+     */
+    ,
+    get: function get() {
+      return (0, _coordinates.glToSystem)([], this._position, this._coordinateSystem);
+    }
+
+    /**
+     * Set listener up direction (not an absolute position). It will update
+     * the relative positions of the sources after a call to the update
+     * method.
+     *
+     * Default value is [0, 1, 0] in 'gl' coordinates.
+     *
+     * @see {@link Listener#update}
+     *
+     * @param {Coordinates} positionRequest
+     */
+
+  }, {
+    key: 'up',
+    set: function set(upRequest) {
+      (0, _coordinates.systemToGl)(this._up, upRequest, this._coordinateSystem);
+      this._outdated = true;
+    }
+
+    /**
+     * Get listener up direction.
+     *
+     * @returns {Coordinates}
+     */
+    ,
+    get: function get() {
+      return (0, _coordinates.glToSystem)([], this._up, this._coordinateSystem);
+    }
+
+    /**
+     * Set listener view, as an aiming position. It is an absolute position,
+     * and not a direction. It will update the relative positions of the
+     * sources after a call to the update method.
+     *
+     * Default value is [0, 0, -1] in 'gl' coordinates.
+     *
+     * @see {@link Listener#update}
+     *
+     * @param {Coordinates} positionRequest
+     */
+
+  }, {
+    key: 'view',
+    set: function set(viewRequest) {
+      (0, _coordinates.systemToGl)(this._view, viewRequest, this._coordinateSystem);
+      this._outdated = true;
+    }
+
+    /**
+     * Get listener view.
+     *
+     * @returns {Coordinates}
+     */
+    ,
+    get: function get() {
+      return (0, _coordinates.glToSystem)([], this._view, this._coordinateSystem);
+    }
+  }]);
+
+  return Listener;
+}();
+
+exports.default = Listener;
